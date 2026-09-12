5a6c9a55325b6340fd40ca54acb4ac908c12bc31
bhaskar Thakur <bhaskar843120@gmail.com>
2026-09-12 16:37:50 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDxaC5hESZV_R2YcYKN1GEMx3ju70Pv838",
-  authDomain:"wessy-sensitivity-85b59.firebaseapp.com",
-  projectId: "wessy-sensitivity",
-  storageBucket: "wessy-sensitivity.firebasestorage.app",
-  messagingSenderId: "97999057231",
-  appId: "1:97999057231:web:65f128e6f32de8bcd1553a",
-  measurementId: "G-HELBLQSK92"
+  apiKey: "AIzaSyBZ5LRVbanoyqbo7xOpiPN2dteclZ6doUk",
+  authDomain: "wessy-sensitivity-85b59.firebaseapp.com",
+  projectId: "wessy-sensitivity-85b59",
+  storageBucket: "wessy-sensitivity-85b59.firebasestorage.app",
+  messagingSenderId: "433244262331",
+  appId: "1:433244262331:web:add0c564c3df839e5a3e9e",
+  measurementId: "G-3BX3CCVZH0"
 };
 
 export const OWNER_EMAILS = [
